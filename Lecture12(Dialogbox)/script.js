e464cdb8d0a75e2e715985369f2ee4355e7f1c61
jhapriya909 <thepriyajha8840@gmail.com>
2025-04-25 10:35:56 +0530

    d:\javascript\Javascript\Lecture12(Dialogbox)\script.js

---
+++ Lecture12(Dialogbox)/script.js
@@ -0,0 +1,6 @@
+// const  alertResult = alert("Your message goes here ");
+
+// const isConfirmed = confirm("Be sure to do your payment after final payment no money will be refunded would you still like to preoceed");
+
+
+const userInput = prompt("Please enter your name : ");
